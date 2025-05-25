30412de3b9ecc45d5edca61dd668cd5ea0f38a28
pr3m19 <69632837+pr3m19@users.noreply.github.com>
2025-05-25 12:32:29 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Hello, GitHub Pages!</h1>
+      <p>This is a sample React app deployed with GitHub Actions.</p>
+    </div>
+  );
+}
+
+export default App;
